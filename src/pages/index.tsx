563d26e8e12c028a0c0996e4affb5ac2d9344a12
caleb-freitas <calebfreitas@tutanota.com>
2022-06-30 14:36:23 -0300

    feat: added home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
-import styles from '../styles/home.module.scss'
+import styles from './home.module.scss'
 import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | ig.news</title>
+        <title>home</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>hello, there. welcome!</span>
+          <h1>news about the <span>react</span> world</h1>
+          <p>
+            get access to all the publication <br />
+            <span>for only $9.90 a month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="girl coding" />
+      </main>
     </>
   )
 }
