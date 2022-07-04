78cc73346c223d89339ed48ce082c9b146491dba
caleb-freitas <calebfreitas@tutanota.com>
2022-07-04 12:42:09 -0300

    feat: added fauna query to add logged user to collection users

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb'
 import NextAuth from "next-auth"
 import GitHubProvider from "next-auth/providers/github"
+import { faunadb } from '../../../services/faunadb'
 
 export default NextAuth({
   providers: [
@@ -7,5 +9,38 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     })
-  ]
+  ],
+  secret: process.env.SIGNING_KEY,
+  callbacks: {
+    async signIn(user) {
+      const { email } = user.user
+      try {
+        await faunadb.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email as string)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email as string)
+              )
+            )
+          )
+        )
+      } catch (error) {
+        return false
+      }
+      return true
+    }
+  }
 })
