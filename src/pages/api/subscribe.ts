2c7c51fbacd1adeccfe185b84b75572a4be83a0f
caleb-freitas <calebfreitas@tutanota.com>
2022-07-07 17:55:32 -0300

    feat: avoided  stripe customer duplication

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,7 +1,19 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { query as q } from "faunadb"
+
 import { stripe } from "../../services/stripe";
+import { faunadb } from "../../services/faunadb";
+
+export type User = {
+  ref: {
+    id: string
+  },
+  data: {
+    stripe_customer_id: string
+  }
+}
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   if (request.method !== "POST") {
@@ -9,11 +21,33 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
     response.status(405).end("Method now allowed")
   }
   const session = await getSession({ req: request })
-  const stripeCustomer = await stripe.customers.create({
-    email: session?.user?.email as string
-  })
+  const user = await faunadb.query<User>(
+    q.Get(
+      q.Match(
+        q.Index('user_by_email'),
+        q.Casefold(session?.user?.email as string)
+      )
+    )
+  )
+  let customerId = user.data.stripe_customer_id
+  if (!customerId) {
+    const stripeCustomer = await stripe.customers.create({
+      email: session?.user?.email as string
+    })
+    await faunadb.query(
+      q.Update(
+        q.Ref(q.Collection('users'), user.ref.id),
+        {
+          data: {
+            stripe_customer_id: stripeCustomer.id
+          }
+        }
+      )
+    )
+    customerId = stripeCustomer.id
+  }
   const stripeCheckoutSession = await stripe.checkout.sessions.create({
-    customer: stripeCustomer.id,
+    customer: customerId,
     payment_method_types: ["card"],
     billing_address_collection: "required",
     line_items: [
